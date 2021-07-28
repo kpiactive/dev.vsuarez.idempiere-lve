8409f72ff62748ae7824d188ab636acaff2feee2
Ing. Victor Suarez <victor.suarez.is@gmail.com>
2021-07-28 17:57:13 -0400

    Se corrige validacion al Completar Factura CxC
    - No dejaba completar sin generar retenciones, lo cual es un error para
    las ventas, en estos casos se Factura antes de Generar la Retencion

--- org.globalqss.idempiere.LVE.withholdings/src/org/globalqss/model/LCO_ValidatorWH.java
+++ org.globalqss.idempiere.LVE.withholdings/src/org/globalqss/model/LCO_ValidatorWH.java
@@ -218,7 +218,7 @@ public class LCO_ValidatorWH extends AbstractEventHandler
 					String genwh = dt.get_ValueAsString("GenerateWithholding");
 					if (genwh != null) {
 
-						if (genwh.equals("Y")) {
+						if (genwh.equals("Y") && !inv.isSOTrx()) {
 							// document type configured to compel generation of withholdings
 							throw new RuntimeException(Msg.getMsg(inv.getCtx(), "LCO_WithholdingNotGenerated"));
 						}
